61cb8d00b011aec8299a723915f76588f41384e0
karibbov <karibbov@gmail.com>
2019-10-24 18:11:22 +0400

    interactivityyyyyy.....

--- Cielab/rect_area.py
+++ Cielab/rect_area.py
@@ -9,15 +9,17 @@ bounderies = list()
 captures = list()
 
 
+
 def load_image(src=src):
     image = cv2.imread(src)
     lab_im = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
     return lab_im
 
 
-def display_image(image):
+def display_image(imag):
+    global image
     # todo: scale the image
-    cv2.imshow("image", image)
+    cv2.imshow("image", imag)
     # cv2.namedWindow('image')
     cv2.setMouseCallback('image', on_mouse)
     cv2.waitKey(0)
@@ -25,7 +27,7 @@ def display_image(image):
 
 
 def on_mouse(event, x, y, flags, p2):
-    global bounds, bounderies
+    global bounds, bounderies, img
     if event == cv2.EVENT_LBUTTONDOWN and len(bounds) < 1:
         x1, y1 = x, y
         bounds['x1'] = x1
@@ -35,19 +37,25 @@ def on_mouse(event, x, y, flags, p2):
         bounds['x2'] = x2
         bounds['y2'] = y2
     if event == cv2.EVENT_RBUTTONDBLCLK:
+        # bounds['x1'] = bounds['x1'] if bounds['x1'] < bounds['x2'] else bounds['x2']
+        # bounds['y1'] = bounds['y1'] if bounds['y1'] < bounds['y2'] else bounds['y2']
+        # bounds['x2'] = bounds['x2'] if bounds['x1'] < bounds['x2'] else bounds['x1']
+        # bounds['y2'] = bounds['y2'] if bounds['x1'] < bounds['x2'] else bounds['y1']
+        cv2.rectangle(img, (bounds['x1'], bounds['y1']), (bounds['x2'], bounds['y2']), (0, 255, 0), 1)
+        cv2.imshow("image", img)
         bounderies.append(bounds)
         bounds = dict()
-        print('ctrl')
+    if event == cv2.EVENT_MBUTTONDBLCLK:
+        bounderies = list()
 
 
 def select(image):
     global captures, bounderies
     for boundery in bounderies:
-        x1 = boundery['x1']
-        x2 = boundery['x2']
-        y1 = boundery['y1']
-        y2 = boundery['y2']
-        # todo: show selection box
+        x1 = boundery['x1'] if boundery['x1'] < boundery['x2'] else boundery['x2']
+        x2 = boundery['x2'] if boundery['x1'] < boundery['x2'] else boundery['x1']
+        y1 = boundery['y1'] if boundery['y1'] < boundery['y2'] else boundery['y2']
+        y2 = boundery['y2'] if boundery['y1'] < boundery['y2'] else boundery['y1']
         roi = image[y1:y2, x1:x2]
         captures.append(roi)
         cv2.imwrite('captured_' + str(len(captures)) + '.png', roi)
@@ -76,6 +84,8 @@ def get_stats(values, pp, title):
     pp.savefig()
 
 
-display_image(load_image())
+image = load_image()
+img = image.copy()
+display_image(img)
 get_image_stats(captures)
 
