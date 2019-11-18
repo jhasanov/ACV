669a203fab88f01dc46d6abc610c7ae2fc8eb4e5
karibbov <karibbov@gmail.com>
2019-11-18 16:02:40 +0400

    image comparisons

--- Cielab/rect_area.py
+++ Cielab/rect_area.py
@@ -2,32 +2,34 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 from matplotlib.backends.backend_pdf import PdfPages
-
 src = 'let_there_be_colour.png'
+size = (1280, 960)
 bounds = dict()
 bounderies = list()
 captures = list()
 
 
-
 def load_image(src=src):
+    global size
     image = cv2.imread(src)
-    lab_im = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+    img_scaled = cv2.resize(image, size, interpolation=cv2.INTER_AREA)
+    lab_im = cv2.cvtColor(img_scaled, cv2.COLOR_BGR2LAB)
     return lab_im
 
 
 def display_image(imag):
     global image
-    # todo: scale the image
     cv2.imshow("image", imag)
-    # cv2.namedWindow('image')
+    cv2.namedWindow('image')
     cv2.setMouseCallback('image', on_mouse)
+    # finish selecting with any key
     cv2.waitKey(0)
     select(image)
 
 
 def on_mouse(event, x, y, flags, p2):
-    global bounds, bounderies, img
+    global bounds, bounderies, img, image
+    # select areas by pushing and holding mouse left
     if event == cv2.EVENT_LBUTTONDOWN and len(bounds) < 1:
         x1, y1 = x, y
         bounds['x1'] = x1
@@ -36,17 +38,18 @@ def on_mouse(event, x, y, flags, p2):
         x2, y2 = x, y
         bounds['x2'] = x2
         bounds['y2'] = y2
+    # show selected areas with right doubleclick
     if event == cv2.EVENT_RBUTTONDBLCLK:
-        # bounds['x1'] = bounds['x1'] if bounds['x1'] < bounds['x2'] else bounds['x2']
-        # bounds['y1'] = bounds['y1'] if bounds['y1'] < bounds['y2'] else bounds['y2']
-        # bounds['x2'] = bounds['x2'] if bounds['x1'] < bounds['x2'] else bounds['x1']
-        # bounds['y2'] = bounds['y2'] if bounds['x1'] < bounds['x2'] else bounds['y1']
         cv2.rectangle(img, (bounds['x1'], bounds['y1']), (bounds['x2'], bounds['y2']), (0, 255, 0), 1)
         cv2.imshow("image", img)
         bounderies.append(bounds)
         bounds = dict()
+    # reset selections using doubleclick of middle button
     if event == cv2.EVENT_MBUTTONDBLCLK:
+        bounds = dict()
         bounderies = list()
+        img = image.copy()
+        cv2.imshow("image", img)
 
 
 def select(image):
@@ -60,20 +63,20 @@ def select(image):
         captures.append(roi)
         cv2.imwrite('captured_' + str(len(captures)) + '.png', roi)
 
-
-def get_image_stats(im_list):
-    name = str(src[:-4] + '.pdf')
-    pp = PdfPages(name)
+name = str(src[:-4] + '.pdf')
+def get_image_stats(im_list, chnl_list=['0', '1', '2'], pdf_path=name):
+    pp = PdfPages(pdf_path)
     for idx in range(3):
         channel = list()
         for imag in im_list:
             chnl = list(np.array(imag[:, :, idx]).flatten())
             channel.extend(chnl)
-        get_stats(channel, pp, str('Channel: ' + str(idx) + ' histogram'))
+        get_histogram(channel, str('Channel: ' + chnl_list[idx] + ' histogram'))
+        pp.savefig()
     pp.close()
 
 
-def get_stats(values, pp, title):
+def get_histogram(values, title):
     std = np.std(values, dtype=np.float64)
     mean = np.mean(values, dtype=np.float64)
     plt.figure()
@@ -81,11 +84,11 @@ def get_stats(values, pp, title):
     # todo: display more informative data
     plt.hist(values, bins='auto')
     plt.title(title + '\nstd: ' + str(std) + '\nmean: ' + str(mean))
-    pp.savefig()
 
-
-image = load_image()
-img = image.copy()
-display_image(img)
-get_image_stats(captures)
+if __name__ == '__main__':
+    image = load_image()
+    img = image.copy()
+    display_image(img)
+    get_image_stats(captures)
+    cv2.destroyAllWindows()
 
---
+++ Cielab/rgb_cielab_rgb.py
@@ -0,0 +1,92 @@
+import numpy as np
+import cv2
+import sys
+import pyautogui
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import tkinter as tk
+from tkinter import filedialog
+from Cielab.rect_area import get_image_stats
+from matplotlib import pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+width, height = pyautogui.size()
+scale = 0.5
+size = int(width*scale), int(height*scale)
+
+def load_image(src):
+    global size
+    image = cv2.imread(src)
+    img_scaled = cv2.resize(image, size, interpolation=cv2.INTER_AREA)
+    # lab_im = cv2.cvtColor(img_scaled, cv2.COLOR_BGR2LAB)
+    return img_scaled
+
+def convert_image(image):
+    lab_im = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+    bgr_im = cv2.cvtColor(lab_im, cv2.COLOR_LAB2BGR)
+    return bgr_im
+
+
+def compare_2_images(image1, image2):
+    std1 = list()
+    std2 = list()
+    mean1 = list()
+    mean2 = list()
+    squared_error = list()
+    for channel in range(image1.shape[2]):
+        im1_channel_data = np.array(image1[:, :, channel]).flatten()
+        im2_channel_data = np.array(image2[:, :, channel]).flatten()
+        std1.append(np.std(im1_channel_data, dtype=np.float64))
+        std2.append(np.std(im2_channel_data, dtype=np.float64))
+        mean1.append(np.mean(im1_channel_data, dtype=np.float64))
+        mean2.append(np.mean(im2_channel_data, dtype=np.float64))
+
+        sq_err = ((im1_channel_data - im2_channel_data)**2).mean()
+        squared_error.append(sq_err)
+
+    # print(squared_error)
+    return mean1, std1, mean2, std2, squared_error
+
+
+if __name__ == '__main__':
+    while True:
+        root = tk.Tk()
+        root.withdraw()
+
+        file_path = filedialog.askopenfilename(title='Select an Image')
+        # print(type(file_path), file_path)
+        try:
+            image = load_image(file_path)
+        except:
+            break
+        converted_image = convert_image(image)
+        print(compare_2_images(image, converted_image))
+        for i in range(10):
+            converted_image = convert_image(converted_image)
+            mean1, std1, mean2, std2, squared_error = compare_2_images(image, converted_image)
+            mean_diff = np.subtract(mean1, mean2)
+            std_diff = np.subtract(std1, std2)
+            print('Iteration : {}\n'.format(i),
+                  'mean1_B: {0} - mean2_B: {1} = {2}\n'.format(mean1[0], mean2[0], mean_diff[0]),
+                  'mean1_G: {0} - mean2_G: {1} = {2}\n'.format(mean1[1], mean2[1], mean_diff[1]),
+                  'mean1_R: {0} - mean2_R: {1} = {2}\n'.format(mean1[2], mean2[2], mean_diff[2]),
+                  'std1_B: {0} - std2_B: {1} = {2}\n'.format(std1[0], std2[0], std_diff[0]),
+                  'std1_G: {0} - std2_G: {1} = {2}\n'.format(std1[1], std2[1], std_diff[1]),
+                  'std1_B: {0} - std2_R: {1} = {2}\n'.format(std1[2], std2[2], std_diff[2]))
+        difference = cv2.subtract(image, converted_image)
+        squared_error = ((image - converted_image)**2)
+        # print(image)
+        images = list()
+        a = np.multiply(image, squared_error)
+        multiplied = np.nan_to_num(np.divide(a, squared_error))
+        # print(multiplied)
+        images.append(multiplied)
+        get_image_stats(images, chnl_list=['Blue', 'Green', 'Red'], pdf_path=file_path[:-4]+'.pdf')
+        # print(difference)
+        # difference[difference == 0] = 255
+        # difference = 255 - difference
+        # print(difference)
+        print(compare_2_images(image, converted_image))
+
+        cv2.imshow("image", multiplied)
+        cv2.waitKey(0)
